c2591a1f4a9255bbec4d29ab6ee1f8441dd3bbb1
JP Posma <jpp@jp-pro-2.lan>
2022-02-11 13:37:17 -0800

    Automated npm deploys on every commit
    This creates a new npm package with the commit hash using Github
    Actions.
    
    For this I also removed the “prepack” step in npm, since we’re already
    building before publishing (and there is no need for publishing manually
    any more).

--- zaplib/scripts/ci/build_web_ci.sh
+++ zaplib/scripts/ci/build_web_ci.sh
@@ -5,11 +5,18 @@ set -euxo pipefail
 # Per https://stackoverflow.com/a/16349776; go to repo root
 cd "${0%/*}/../../.."
 
-# Build test suite as it is getting run in Jest
-cargo zaplib build -p test_suite
-
+## Build
 cd zaplib/web
 yarn install
-yarn lint
 yarn build
+
+## Test
+yarn lint
+
+# Build test suite as it is getting run in Jest
+cargo zaplib build -p test_suite
 yarn run jest --forceExit
+
+## Publish
+npm version 0.0.0-$(git rev-parse --short HEAD)
+npm publish --tag canary
