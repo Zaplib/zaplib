92780125f33260d1503afc4c49d9499d5fb0766c
JP Posma <janpaul123@users.noreply.github.com>
2022-02-23 10:15:38 -0800

    Remove separate nightly build, since all our builds use nightly now
    I did preserve a comment for if we ever switch away from nightly. :)

--- zaplib/scripts/ci/debug_builds_native.sh
+++ zaplib/scripts/ci/debug_builds_native.sh
@@ -8,4 +8,9 @@ cd "${0%/*}/../../.."
 zaplib/scripts/ci/common.sh
 
 export RUSTFLAGS="-D warnings"
-cargo build --all-targets --workspace # builds everything in the workspace, including tests, etc
+
+# Builds everything in the workspace, including tests, etc.
+# Note that it's important that this is a nightly build (which it currently is through
+# rust-toolchain.toml), so we get notified about features that have stabilized, like
+# https://github.com/rust-lang/rust/issues/58179#issuecomment-867793443
+cargo build --all-targets --workspace
