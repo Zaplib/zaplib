3ffba5971df815010da279126020a54e13431b0a
JP Posma <janpaul123@users.noreply.github.com>
2022-02-23 10:09:34 -0800

    Split out browser_tests.sh from build_web_ci.sh
    Since we’re going to add a bunch more browser tests.

--- zaplib/scripts/ci/build_web_ci.sh
+++ zaplib/scripts/ci/build_web_ci.sh
@@ -26,22 +26,3 @@ pushd zaplib/web
     # Run jest (uses test suite)
     yarn run jest --forceExit
 popd
-
-# Integration tests with Browserstack (uses test suite)
-# Local identifier is necessary to be able to run multiple jobs in parallel.
-export BROWSERSTACK_LOCAL_IDENTIFIER=$(echo $RANDOM$RANDOM$RANDOM)
-BrowserStackLocal --key $BROWSERSTACK_KEY --debug-utility --daemon start --local-identifier $BROWSERSTACK_LOCAL_IDENTIFIER
-cargo run -p zaplib_ci -- --webdriver-url "https://jpposma_0ZuiXP:${BROWSERSTACK_KEY}@hub-cloud.browserstack.com/wd/hub" --browserstack-local-identifier $BROWSERSTACK_LOCAL_IDENTIFIER
-
-# Screenshots are saved in `screenshots/`. Previous ones in `previous_screenshots/`. Let's compare!
-# `--ignoreChange` makes it so this call doesn't fail when there are changed screenshots; we don't
-# want to block merging in that case.
-# TODO(JP): We do want to add an automatic comment being posted to Github, so someone can review the
-# screenshots.
-zaplib/web/node_modules/.bin/reg-cli screenshots/ previous_screenshots/ diff_screenshots/ -R ./index.html --ignoreChange
-# Now let's bundle everything up in screenshots_report/
-mkdir screenshots_report/
-mv index.html screenshots_report/
-mv screenshots/ screenshots_report/
-mv previous_screenshots/ screenshots_report/
-mv diff_screenshots/ screenshots_report/
