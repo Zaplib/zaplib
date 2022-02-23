0fd2ea17fd0e509ce45a49fb3748c5fd41105166
JP Posma <janpaul123@users.noreply.github.com>
2022-02-23 10:03:16 -0800

    Split up builds CI jobs
    They were taking by far the longest.

--- zaplib/scripts/ci/debug_builds_native.sh
+++ zaplib/scripts/ci/debug_builds_native.sh
@@ -9,4 +9,3 @@ zaplib/scripts/ci/common.sh
 
 export RUSTFLAGS="-D warnings"
 cargo build --all-targets --workspace # builds everything in the workspace, including tests, etc
-cargo run -p cargo-zaplib -- build --all-targets --workspace # same but for wasm
---
+++ zaplib/scripts/ci/debug_builds_wasm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Per https://stackoverflow.com/a/16349776; go to repo root
+cd "${0%/*}/../../.."
+
+zaplib/scripts/ci/common.sh
+
+export RUSTFLAGS="-D warnings"
+cargo run -p cargo-zaplib -- build --all-targets --workspace # # builds everything in the workspace, including tests, etc for wasm
