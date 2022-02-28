9999e0566422cdce5f51438139c281e6ffdd1941
JP Posma <janpaul123@users.noreply.github.com>
2022-02-28 13:57:44 -0800

    Upload screenshots to S3 and post comment on Github
    The comment is posted on the commit itself, which should show up in PRs.
    
    The one open question I have is how to deal with “auto merge” in Github.
    It could make actual regressions go unnoticed. Any ideas?

--- zaplib/scripts/ci/browser_tests.sh
+++ zaplib/scripts/ci/browser_tests.sh
@@ -24,12 +24,20 @@ cargo run -p zaplib_ci -- --webdriver-url "https://jpposma_0ZuiXP:${BROWSERSTACK
 # Screenshots are saved in `screenshots/`. Previous ones in `previous_screenshots/`. Let's compare!
 # `--ignoreChange` makes it so this call doesn't fail when there are changed screenshots; we don't
 # want to block merging in that case.
-# TODO(JP): We do want to add an automatic comment being posted to Github, so someone can review the
-# screenshots.
-zaplib/web/node_modules/.bin/reg-cli screenshots/ previous_screenshots/ diff_screenshots/ -R ./index.html --ignoreChange
+zaplib/web/node_modules/.bin/reg-cli screenshots/ previous_screenshots/ diff_screenshots/ --report ./index.html --json ./reg.json --ignoreChange --enableAntialias
 # Now let's bundle everything up in screenshots_report/
 mkdir screenshots_report/
 mv index.html screenshots_report/
+mv reg.json screenshots_report/
 mv screenshots/ screenshots_report/
 mv previous_screenshots/ screenshots_report/
 mv diff_screenshots/ screenshots_report/
+aws s3 cp --recursive screenshots_report/ s3://zaplib-screenshots/$GITHUB_SHA
+
+if grep --fixed-strings '"newItems":[]' screenshots_report/reg.json && grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json
+then
+  echo "SCREENSHOT_GITHUB_MESSAGE=[✅ No screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA)" >> $GITHUB_ENV
+else
+  echo "SCREENSHOT_GITHUB_MESSAGE=[🤔 Screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) Please look at the screenshots and tag this comment with 👍 or 👎. Only merge when both the PR author and all the reviewers are happy with the changes." >> $GITHUB_ENV
+fi
+
