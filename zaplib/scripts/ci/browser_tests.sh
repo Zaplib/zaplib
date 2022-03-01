fafaa821ef13f7934a167da1b4cc3f730f63a3de
JP Posma <janpaul123@users.noreply.github.com>
2022-02-28 18:38:31 -0800

    Add screenshots of examples in CI
    Not all screenshots work yet, so I added some TODOs. Makes progress
    towards #29.
    
    Samsung Galaxy has some issues, so I excluded it for the examples, and
    added a note in #67.

--- zaplib/scripts/ci/browser_tests.sh
+++ zaplib/scripts/ci/browser_tests.sh
@@ -5,8 +5,29 @@ set -euxo pipefail
 # Per https://stackoverflow.com/a/16349776; go to repo root
 cd "${0%/*}/../../.."
 
-# Build test_suite.wasm
+# Build the wasm packages that we need in tests.
 cargo run -p cargo-zaplib -- build -p test_suite
+cargo run -p cargo-zaplib -- build -p tutorial_2d_rendering_step1
+cargo run -p cargo-zaplib -- build -p tutorial_2d_rendering_step2
+cargo run -p cargo-zaplib -- build -p tutorial_2d_rendering_step3
+cargo run -p cargo-zaplib -- build -p tutorial_3d_rendering_step2
+cargo run -p cargo-zaplib -- build -p tutorial_3d_rendering_step3
+cargo run -p cargo-zaplib -- build -p tutorial_hello_thread
+cargo run -p cargo-zaplib -- build -p tutorial_hello_world_canvas
+cargo run -p cargo-zaplib -- build -p tutorial_hello_world_console
+cargo run -p cargo-zaplib -- build -p tutorial_js_rust_bridge
+cargo run -p cargo-zaplib -- build -p tutorial_ui_components
+cargo run -p cargo-zaplib -- build -p tutorial_ui_layout
+cargo run -p cargo-zaplib -- build --release -p example_charts
+cargo run -p cargo-zaplib -- build --release -p example_lots_of_buttons
+cargo run -p cargo-zaplib -- build --release -p example_shader
+cargo run -p cargo-zaplib -- build --release -p example_single_button
+cargo run -p cargo-zaplib -- build --release -p example_text
+cargo run -p cargo-zaplib -- build --release -p test_bottom_bar
+cargo run -p cargo-zaplib -- build --release -p test_geometry
+cargo run -p cargo-zaplib -- build --release -p test_layout
+cargo run -p cargo-zaplib -- build --release -p test_padding
+cargo run -p cargo-zaplib -- build --release -p test_popover
 
 # Build
 pushd zaplib/web
