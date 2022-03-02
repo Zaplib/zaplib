ba66d192e73ce317c297f5851a056816804abc11
JP Posma <janpaul123@users.noreply.github.com>
2022-03-02 03:07:15 +0000

    Add special message for if only new screenshots have been found
    This is pretty common so I figured it might be nice to have some
    guidance.

--- zaplib/scripts/ci/browser_tests.sh
+++ zaplib/scripts/ci/browser_tests.sh
@@ -59,6 +59,11 @@ if grep --fixed-strings '"newItems":[]' screenshots_report/reg.json && grep --fi
 then
   echo "SCREENSHOT_GITHUB_MESSAGE=[✅ No screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA)" >> $GITHUB_ENV
 else
-  echo "SCREENSHOT_GITHUB_MESSAGE=[🤔 Screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) Please look at the screenshots and tag this comment with 👍 or 👎. Only merge when both the PR author and a reviewer are happy with the changes." >> $GITHUB_ENV
+  if grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"passedItems":[]' screenshots_report/reg.json
+  then
+    echo "SCREENSHOT_GITHUB_MESSAGE=[⚠️ Only new screenshots found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) This typically happens when the base commit screenshots were not built yet; just rerun the workspace. If that doesn't help, please contact a maintainer for help." >> $GITHUB_ENV
+  else
+    echo "SCREENSHOT_GITHUB_MESSAGE=[🤔 Screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) Please look at the screenshots and tag this comment with 👍 or 👎. Only merge when both the PR author and a reviewer are happy with the changes." >> $GITHUB_ENV
+  fi
 fi
 
