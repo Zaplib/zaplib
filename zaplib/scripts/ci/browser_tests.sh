74ac4c78ca8a0eb1ac3f89184d0ddbd8c09ab400
JP Posma <janpaul123@users.noreply.github.com>
2022-03-02 18:58:49 +0000

    Wait until after tests before downloading previous screenshots
    To give the main branch a bit more time to upload, if it was just
    merged.

--- zaplib/scripts/ci/browser_tests.sh
+++ zaplib/scripts/ci/browser_tests.sh
@@ -43,29 +43,3 @@ popd
 export BROWSERSTACK_LOCAL_IDENTIFIER=$(echo $RANDOM$RANDOM$RANDOM)
 BrowserStackLocal --key $BROWSERSTACK_KEY --debug-utility --daemon start --local-identifier $BROWSERSTACK_LOCAL_IDENTIFIER
 cargo run -p zaplib_ci -- --webdriver-url "https://jpposma_0ZuiXP:${BROWSERSTACK_KEY}@hub-cloud.browserstack.com/wd/hub" --browserstack-local-identifier $BROWSERSTACK_LOCAL_IDENTIFIER
-
-# Screenshots are saved in `screenshots/`. Previous ones in `previous_screenshots/`. Let's compare!
-# `--ignoreChange` makes it so this call doesn't fail when there are changed screenshots; we don't
-# want to block merging in that case.
-zaplib/web/node_modules/.bin/reg-cli screenshots/ previous_screenshots/ diff_screenshots/ --report ./index.html --json ./reg.json --ignoreChange --enableAntialias --matchingThreshold 0.05
-# Now let's bundle everything up in screenshots_report/
-mkdir screenshots_report/
-mv index.html screenshots_report/
-mv reg.json screenshots_report/
-mv screenshots/ screenshots_report/
-mv previous_screenshots/ screenshots_report/
-mv diff_screenshots/ screenshots_report/
-aws s3 cp --recursive screenshots_report/ s3://zaplib-screenshots/$GITHUB_SHA
-
-if grep --fixed-strings '"newItems":[]' screenshots_report/reg.json && grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json
-then
-  echo "SCREENSHOT_GITHUB_MESSAGE=[✅ No screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA)" >> $GITHUB_ENV
-else
-  if grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"passedItems":[]' screenshots_report/reg.json
-  then
-    echo "SCREENSHOT_GITHUB_MESSAGE=[⚠️ Only new screenshots found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) This typically happens when the base commit screenshots were not built yet; just rerun the workspace. If that doesn't help, please contact a maintainer for help." >> $GITHUB_ENV
-  else
-    echo "SCREENSHOT_GITHUB_MESSAGE=[🤔 Screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) Please look at the screenshots and tag this comment with 👍 or 👎. Only merge when both the PR author and a reviewer are happy with the changes." >> $GITHUB_ENV
-  fi
-fi
-
---
+++ zaplib/scripts/ci/browser_tests_upload_screenshots.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+# Per https://stackoverflow.com/a/16349776; go to repo root
+cd "${0%/*}/../../.."
+
+# Screenshots are saved in `screenshots/`. Previous ones in `previous_screenshots/`. Let's compare!
+# `--ignoreChange` makes it so this call doesn't fail when there are changed screenshots; we don't
+# want to block merging in that case.
+zaplib/web/node_modules/.bin/reg-cli screenshots/ previous_screenshots/ diff_screenshots/ --report ./index.html --json ./reg.json --ignoreChange --enableAntialias --matchingThreshold 0.05
+# Now let's bundle everything up in screenshots_report/
+mkdir screenshots_report/
+mv index.html screenshots_report/
+mv reg.json screenshots_report/
+mv screenshots/ screenshots_report/
+mv previous_screenshots/ screenshots_report/
+mv diff_screenshots/ screenshots_report/
+aws s3 cp --recursive screenshots_report/ s3://zaplib-screenshots/$GITHUB_SHA
+
+if grep --fixed-strings '"newItems":[]' screenshots_report/reg.json && grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json
+then
+  echo "SCREENSHOT_GITHUB_MESSAGE=[✅ No screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA)" >> $GITHUB_ENV
+else
+  if grep --fixed-strings '"deletedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"failedItems":[]' screenshots_report/reg.json && grep --fixed-strings '"passedItems":[]' screenshots_report/reg.json
+  then
+    echo "SCREENSHOT_GITHUB_MESSAGE=[⚠️ Only new screenshots found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) This typically happens when the base commit screenshots were not built yet; just rerun the workspace. If that doesn't help, please contact a maintainer for help." >> $GITHUB_ENV
+  else
+    echo "SCREENSHOT_GITHUB_MESSAGE=[🤔 Screenshot diffs found.](http://zaplib-screenshots.s3-website-us-east-1.amazonaws.com/$GITHUB_SHA) Please look at the screenshots and tag this comment with 👍 or 👎. Only merge when both the PR author and a reviewer are happy with the changes." >> $GITHUB_ENV
+  fi
+fi
+
