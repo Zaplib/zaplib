8d81274a7afab592381eefc8d8d36fa0ce52fc2e
Steve Krouse <steveykrouse@gmail.com>
2022-03-16 15:14:48 -0700

    fix screenshot bug

--- zaplib/ci/src/cmd.rs
+++ zaplib/ci/src/cmd.rs
@@ -228,6 +228,7 @@ async fn run_tests(webdriver_url: String, local_port: u16, browserstack_local_id
         capabilities.add("acceptSslCerts", true).unwrap();
         let mut driver = WebDriver::new(&webdriver_url, &capabilities).await.unwrap();
         test_suite_all_tests_3x("local browser", &mut driver, local_port).await.unwrap();
+        examples_screenshots("local browser", &mut driver, local_port).await.unwrap();
         driver.quit().await.unwrap();
     }
 }
@@ -298,7 +299,9 @@ async fn examples_screenshots(browser_name: &str, driver: &mut WebDriver, local_
         let script = r#"
             const done = arguments[0];
             const interval = setInterval(() => {
-                if (zaplib.isInitialized()) {
+                // tutorial_3d_rendering/step1 doesn't even import zaplib
+                // so if zaplib is undefined, continue
+                if (!window.zaplib || zaplib.isInitialized()) {
                     clearInterval(interval);
                     setTimeout(() => {
                         done("SUCCESS");
